709fa3e3af27d92a27664b3ec55038dd9cb1dbed
GladieyThor <132149683+GladieyThor@users.noreply.github.com>
2023-09-23 12:42:39 -0300

    Add files via upload

---
+++ Estrutura de Repetição While/ex2.c
@@ -0,0 +1,19 @@
+/*
+2) Faça um algoritmo que exiba todos números de 100 a 1.
+*/
+
+
+#include <stdio.h>
+
+
+int main(){
+
+    int i = 100;
+
+    while(i >= 1){
+        printf("\n%d", i);
+        i = i - 1;
+    }//ENQUANTO i FOR MAIOR QUE 1, REALIZA A EXIBIÇÃO DO VALOR;
+
+    return 0;
+}//MAIN;
