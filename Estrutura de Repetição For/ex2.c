ef1a2b0b8a1fe5f3b01260c5e179f53db45147e5
GladieyThor <132149683+GladieyThor@users.noreply.github.com>
2023-09-30 13:11:14 -0300

    Add files via upload

---
+++ Estrutura de Repetição For/ex2.c
@@ -0,0 +1,14 @@
+/*
+2) Faça um algoritmo que exiba todos números de 100 a 1.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    for(int i = 100; i >= 1; i--){
+        printf("\n%d", i);
+    }//FOR PARA EXIBIÇÃO DOS VALORES DE 100 A 1;
+
+    return 0;
+}//MAIN;
